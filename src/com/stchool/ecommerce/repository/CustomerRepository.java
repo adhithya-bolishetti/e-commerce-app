215cc09c8fee84e01840d43c26e4fa4816f1f8ce
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-02-11 22:52:29 +0530

    feat: Fixed logic of addNewCustomer method

--- src/com/stchool/ecommerce/repository/CustomerRepository.java
+++ src/com/stchool/ecommerce/repository/CustomerRepository.java
@@ -5,6 +5,7 @@ import com.stchool.ecommerce.model.Customer;
 public class CustomerRepository {
     public Customer addNewCustomer(Customer customer) {
         System.out.println("New Customer Added Successfully");
+        System.out.println("Handling signup data of " + customer + " in repository layer");
         return customer;
     }
 }
