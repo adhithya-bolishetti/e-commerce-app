c26865522edc310b81e6663d6688f0a3d40e783f
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-05-20 11:14:31 +0530

    feat: Override methods to implement CRUD methods

--- src/com/stchool/ecommerce/controller/CustomerController.java
+++ src/com/stchool/ecommerce/controller/CustomerController.java
@@ -1,11 +1,10 @@
 package com.stchool.ecommerce.controller;
 
 import com.stchool.ecommerce.model.Customer;
-import com.stchool.ecommerce.service.CustomerService;
 
 public class CustomerController {
     public Customer handleSignup(Customer customer) {
-        System.out.println("Handling Signup data of " + customer + " in Controller layer");
+        System.out.println("Handling Signup data of " + customer.getName() + " in Controller layer");
 
         CustomerService customerService = new CustomerService();
         return customerService.handleSignup(customer);
