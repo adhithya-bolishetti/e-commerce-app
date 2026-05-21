aaa4472d5a90be967045a57b27d2741e196d1736
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-05-21 17:55:21 +0530

    feat: Add AppFactory class get Input using Scanner object

---
+++ src/com/stchool/ecommerce/util/InputUtil.java
@@ -0,0 +1,36 @@
+package com.stchool.ecommerce.util;
+
+import java.util.Scanner;
+
+public class InputUtil {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int getIntInput(String message) {
+        System.out.println(message);
+        return scanner.nextInt();
+    }
+
+    public static String getStringInput(String message) {
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+
+    public static char getCharInput(String message) {
+        System.out.println(message);
+        return scanner.next().charAt(0);
+    }
+
+    public static float getFloatInput(String message) {
+        System.out.println(message);
+        return scanner.nextFloat();
+    }
+
+    public static double getDoubleInput(String message) {
+        System.out.println(message);
+        return scanner.nextDouble();
+    }
+
+    public static void closeScanner() {
+        scanner.close();
+    }
+}
